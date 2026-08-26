28532206a58e381c2b1b12f67cf4219e973b2a6d
Yuxing Hu <yuxihu@microsoft.com>
2026-08-26 16:08:17 +0800

    Remove farewell website

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import FarewellPage from './FarewellPage';
 import JourneysPage from './JourneysPage';
 import './index.css';
 import { findTripByPath } from './travel/trips';
@@ -9,10 +8,6 @@ import { findTripByPath } from './travel/trips';
 const activeTrip = findTripByPath(window.location.pathname);
 const activePath = window.location.pathname.replace(/^\/+|\/+$/g, '');
 const isJourneysPage = activePath === 'journeys';
-const isStandaloneFarewellTemplate = Boolean(
-  (window as Window & { __FAREWELL_TEMPLATE__?: boolean }).__FAREWELL_TEMPLATE__,
-);
-const isFarewellPage = activePath === 'farewell' || isStandaloneFarewellTemplate;
 const TravelPage = React.lazy(() => import('./travel/TravelPage'));
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -21,8 +16,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <React.Suspense fallback={null}>
         <TravelPage key={activeTrip.slug} trip={activeTrip} />
       </React.Suspense>
-    ) : isFarewellPage ? (
-      <FarewellPage />
     ) : isJourneysPage ? (
       <JourneysPage />
     ) : (
